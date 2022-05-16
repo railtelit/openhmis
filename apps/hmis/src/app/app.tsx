e13157ac4316dc1ca79c745d67d2753e63ea322e
Apple <apple@Apples-MacBook-Pro.local>
2022-05-16 23:10:47 +0530

    App Init

--- apps/hmis/src/app/app.tsx
+++ apps/hmis/src/app/app.tsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
-
+import Button  from '@mui/material/Button'
 export function App() {
   return (
     <>
-      <NxWelcome title="hmis" />
+      <nav>
+        <h3>HMIS</h3>
+      </nav>
       <div />
     </>
   );
