13fdd5fa93e2106a7f6a09b36a1387d044e6dd86
rakeshpagi <rakesh_prog@yahoo.co.in>
2022-09-26 19:54:48 +0530

    admin Service k8 Setup

--- apps/admin/src/environments/environment.prod.ts
+++ apps/admin/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   KC_CLIENT_ID:'react-app',
   KC_REALM:'openhmis-admin',
   KC_URL:'https://auth.openhmis.erpapps.in/',
-  API_ENDPOINT:'https://api.openhmis.erpapps.in'
+  API_ENDPOINT:'https://api.openhmis.erpapps.in/admin'
 };
--- apps/admin/src/environments/environment.ts
+++ apps/admin/src/environments/environment.ts
@@ -6,5 +6,5 @@ export const environment = {
   KC_CLIENT_ID:'react-app',
   KC_REALM:'openhmis-admin',
   KC_URL:'https://auth.openhmis.erpapps.in/',
-  API_ENDPOINT:'http://localhost:3800/api'
+  API_ENDPOINT:'http://localhost:3800/admin'
 };
